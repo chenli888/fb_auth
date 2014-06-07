0a8efb290f53b60ed238103f0f0fb7c0fc5ddb68
Li Chen <philosopherchenli@yahoo.com>
2014-06-07 11:32:02 -0400

    remove redirect

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,11 +2,9 @@ class SessionsController < ApplicationController
   def create
     user = User.from_omniauth(env["omniauth.auth"])
     session[:user_id] = user.id
-    redirect_to root_url
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to root_url
   end
 end
